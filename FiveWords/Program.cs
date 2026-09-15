7a74349e9845ef219d221dccc140b81a66b67368
emilh <61194@edu.sde.dk>
2026-09-15 13:38:21 +0200

    Credit to 1kb-dev for his efforts to finding this BIG issue.

--- FiveWords/Program.cs
+++ FiveWords/Program.cs
@@ -15,7 +15,8 @@ namespace FiveWords
             int wordLength = 5;
             bool onlyUniques = true;
             //ilePath = "C:\\Users\\admin\\source\\repos\\FiveWords\\FiveWords\\words_beta.txt";
-            filePath = "C:\\Users\\admin\\source\\repos\\FiveWords\\FiveWords\\words_alpha.txt";
+            //filePath = "C:\\Users\\admin\\source\\repos\\FiveWords\\FiveWords\\words_alpha.txt";
+            filePath = Path.Combine(AppContext.BaseDirectory, "words_alpha.txt");
 
             DataHandler dh = new DataHandler();           
             List<List<string>>  combinations = dh.GetCombinations(filePath, wordLength, onlyUniques, 5);
